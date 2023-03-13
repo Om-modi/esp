7acf443091e17fe6393f96a14408085a9f5ff48e
om-modi <you@example.com>
2023-03-13 19:01:39 +0530

    upload

--- calc.js
+++ calc.js
@@ -6,7 +6,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
+app.use(express.static('assets'))
 app.get('/', (req, res) => {
   res.render('calc')
 })
